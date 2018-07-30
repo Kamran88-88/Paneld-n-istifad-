a6f3b511a26556c135be0e239cfc5c8cd8a4cc0c
Kamran Aliyev <kamran88-88@outlook.com>
2018-07-30 21:08:32 +0400

    Add project files.

---
+++ WindowsFormsApp16/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace WindowsFormsApp16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Save = new System.Windows.Forms.Button();
+            this.Work_adress = new System.Windows.Forms.TextBox();
+            this.Home_adress = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Next2 = new System.Windows.Forms.Button();
+            this.Mail = new System.Windows.Forms.TextBox();
+            this.Phone = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Next1 = new System.Windows.Forms.Button();
+            this.Soyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.Next1);
+            this.panel1.Controls.Add(this.Soyad);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Ad);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.Next2);
+            this.panel2.Controls.Add(this.Mail);
+            this.panel2.Controls.Add(this.Phone);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 450);
+            this.panel2.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.Save);
+            this.panel3.Controls.Add(this.Work_adress);
+            this.panel3.Controls.Add(this.Home_adress);
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 450);
+            this.panel3.TabIndex = 6;
+            // 
+            // Save
+            // 
+            this.Save.Enabled = false;
+            this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Save.Location = new System.Drawing.Point(669, 395);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(103, 30);
+            this.Save.TabIndex = 5;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Work_adress
+            // 
+            this.Work_adress.Location = new System.Drawing.Point(182, 95);
+            this.Work_adress.Name = "Work_adress";
+            this.Work_adress.Size = new System.Drawing.Size(456, 20);
+            this.Work_adress.TabIndex = 4;
+            this.Work_adress.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Home_adress
+            // 
+            this.Home_adress.Location = new System.Drawing.Point(182, 54);
+            this.Home_adress.Name = "Home_adress";
+            this.Home_adress.Size = new System.Drawing.Size(456, 20);
+            this.Home_adress.TabIndex = 3;
+            this.Home_adress.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(15, 95);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(120, 24);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Work adress:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(8, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 24);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Home adress:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(4, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 24);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Address info";
+            // 
+            // Next2
+            // 
+            this.Next2.Enabled = false;
+            this.Next2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Next2.Location = new System.Drawing.Point(669, 395);
+            this.Next2.Name = "Next2";
+            this.Next2.Size = new System.Drawing.Size(103, 30);
+            this.Next2.TabIndex = 5;
+            this.Next2.Text = "Next";
+            this.Next2.UseVisualStyleBackColor = true;
+            this.Next2.Click += new System.EventHandler(this.Next2_Click);
+            // 
+            // Mail
+            // 
+            this.Mail.Location = new System.Drawing.Point(130, 100);
+            this.Mail.Name = "Mail";
+            this.Mail.Size = new System.Drawing.Size(222, 20);
+            this.Mail.TabIndex = 4;
+            this.Mail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Phone
+            // 
+            this.Phone.Location = new System.Drawing.Point(130, 55);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(222, 20);
+            this.Phone.TabIndex = 3;
+            this.Phone.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(15, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 24);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Mail adres:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(74, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 24);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Tel:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(3, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Contact info";
+            // 
+            // Next1
+            // 
+            this.Next1.Enabled = false;
+            this.Next1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Next1.Location = new System.Drawing.Point(661, 395);
+            this.Next1.Name = "Next1";
+            this.Next1.Size = new System.Drawing.Size(111, 32);
+            this.Next1.TabIndex = 5;
+            this.Next1.Text = "Next";
+            this.Next1.UseVisualStyleBackColor = true;
+            this.Next1.Click += new System.EventHandler(this.Next1_Click);
+            // 
+            // Soyad
+            // 
+            this.Soyad.Location = new System.Drawing.Point(99, 100);
+            this.Soyad.Name = "Soyad";
+            this.Soyad.Size = new System.Drawing.Size(225, 20);
+            this.Soyad.TabIndex = 4;
+            this.Soyad.TextChanged += new System.EventHandler(this.Ad_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ad";
+            // 
+            // Ad
+            // 
+            this.Ad.Location = new System.Drawing.Point(99, 60);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(225, 20);
+            this.Ad.TabIndex = 1;
+            this.Ad.TextChanged += new System.EventHandler(this.Ad_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Personal info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Next1;
+        private System.Windows.Forms.TextBox Soyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Ad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox Mail;
+        private System.Windows.Forms.TextBox Phone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Next2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox Work_adress;
+        private System.Windows.Forms.TextBox Home_adress;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp16/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp16
+{
+    public partial class Form1 : Form
+    {
+        List<Person> people = new List<Person>();
+        Person person = new Person();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Ad_TextChanged(object sender, EventArgs e)
+        {
+            if (Ad.Text != "" && Soyad.Text != "")
+            {
+                Next1.Enabled = true;
+                
+            }
+            else
+            {
+                Next1.Enabled = false;
+            }
+        }
+
+        private void Next1_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = true;
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) //tel and mail
+        {
+            if (Phone.Text!=""&&Mail.Text!="")
+            {
+                Next2.Enabled = true;
+            }
+            else
+            {
+                Next2.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            panel3.Visible = false;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e) 
+        {
+            if (Home_adress.Text!=""&&Work_adress.Text!="")
+            {
+                Save.Enabled = true;
+            }
+            else
+            {
+                Save.Enabled = false;
+            }
+
+           
+            
+        }
+
+        private void Next2_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = true;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            person.name = Ad.Text;
+            person.Surname = Soyad.Text;
+            person.phone = Phone.Text;
+            person.mail = Mail.Text;
+            person.home_adr = Home_adress.Text;
+            person.work_adr = Work_adress.Text;
+
+            people.Add(person);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Ad: {person.name}");
+            stringBuilder.AppendLine($"Soyad: {person.Surname}");
+            stringBuilder.AppendLine($"Tel: {person.phone}");
+            stringBuilder.AppendLine($"Mail: {person.mail}");
+            stringBuilder.AppendLine($"Ev ünvanı: {person.home_adr}");
+            stringBuilder.AppendLine($"İş ünvanı: {person.work_adr}");
+
+
+            MessageBox.Show(stringBuilder.ToString(), "Məlumatlar");
+            Ad.Clear();
+            Soyad.Clear();
+            Phone.Clear();
+            Mail.Clear();
+            Work_adress.Clear();
+            Home_adress.Clear();
+
+            panel3.Visible = false;
+            panel2.Visible = false;
+
+        }
+    }
+
+    class Person
+    {
+        public string name { get; set; }
+        public string Surname { get; set; }
+        public string phone { get; set; }
+        public string mail { get; set; }
+        public string home_adr { get; set; }
+        public string work_adr { get; set; }
+
+    }
+}
